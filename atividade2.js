532464fd48b8220be2bba0a5d37b4f923a6ac67a
lgongra11 <lucasgongra11@gmail.com>
2026-06-11 16:14:16 -0300

    Adiciona atividade2

---
+++ atividade2.js
@@ -0,0 +1,6 @@
+let frutas = ["maçã", "banana", "laranja", "uva", "abacaxi"];
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+console.log(frutas[3]);
+console.log(frutas[4]);
