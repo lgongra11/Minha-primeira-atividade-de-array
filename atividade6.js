99fdc1eac09e245009666a7d2b279a0f00a7d52e
lgongra11 <lucas.s.oliveira14@edu.senai.br>
2026-06-16 11:29:08 -0300

    Adiciona atividade6.js e atividade7.js

--- atividade6.js
+++ atividade6.js
@@ -1,1 +1,4 @@
-let jogos ['Fortnite' ]
+let jogos = ['Fortnite', 'The Last Of Us', 'Call Of Duty'];
+console.log(jogos);
+jogos.push('Clash Royale');
+console.log(jogos);
---
+++ atividade7.js
@@ -0,0 +1,4 @@
+let matérias = ['Matemática', 'Inglês', 'Biologia'];
+console.log(matérias);
+matérias.unshift('Geografia');
+console.log(matérias)
