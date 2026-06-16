33ec1aec8f70ed0040a228fa4a750f566ddd7a85
lgongra11 <lucas.s.oliveira14@edu.senai.br>
2026-06-16 11:52:13 -0300

    Adiciona atividade11 e 12.js

---
+++ atividade11.js
@@ -0,0 +1,3 @@
+let alunos = ['lucas', 'leonardo', 'malu', 'augusto', 'yasmin'];
+for (let i = 0; i < 5; i++)
+    console.log(alunos[i])
---
+++ atividade12.js
@@ -0,0 +1,3 @@
+let filmes = ['Se não fosse você', 'Marley e eu', 'Justiça Virtual', 'Homem aranha', 'Vovozona'];
+for (let i = 0; i < 5; i++);
+    console.log('1-' + filmes[i]);
